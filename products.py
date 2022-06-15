cbfd2f14946d9e2efd256187c42db4f5b7c7b5e4
unknown <w39354878@gmail.com>
2022-06-15 21:43:29 +0800

    結案

--- products.py
+++ products.py
@@ -1,21 +1,27 @@
-#讀取檔案
-#寫入跟讀取檔案都要用同一種編碼
-products = []
-with open('products.csv', 'r', encoding = 'utf-8') as f:
-	for line in f:
-		if '商品, 價格' in line:
-			continue
-			#跳到下一個迴圈
-			#continue 跟 break 一樣只能放在迴圈裏面
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
-		#一個line是一行，就是一個字串
-		#split(',')代表當他遇到逗號(,)就要進行切割
-		#split切割完的東西會變成清單
+import os #operating system 作業系統=電腦的政府的意思
 
-#讓使用者輸入
+#讀取檔案
 products = []
+if os.path.isfile('products.csv'):#檢查檔案在不在
+	print('找到檔案了!!!')
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+					#跳到下一個迴圈
+					#continue 跟 break 一樣只能放在迴圈裏面
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+else:
+	print('找不到檔案.....')
+
+	#寫入跟讀取檔案都要用同一種編碼(encoding='utf-8')
+	#一個line是一行，就是一個字串
+	#split(',')代表當他遇到逗號(,)就要進行切割
+	#split切割完的東西會變成清單
+
+#讓使用者輸入商品清單
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':#quit
@@ -30,17 +36,16 @@ while True:
 	#上面四行相當於 products.append([name, price])
 print(products)
 
-products[0][0]#把大清單(products)中的第一個小清單(p)的東西拿出來
-# 二維清單
+	#把大清單(products)中的第一個小清單(p)的東西拿出來
+	#二維清單
 
 #印出所有購買紀錄
 for p in products:
-	print(p)
 	print(p[0], '的價格是' , p[1])
 	#只印出每一個小清單中的第幾個名字
 
 #寫入檔案
-with open('products.csv', 'w', encoding = 'utf-8') as f:
+with open('products.csv', 'w', encoding='utf-8') as f:
 	#csv 是通融性最高
 	f.write('商品, 價格\n')
 	for p in products:
